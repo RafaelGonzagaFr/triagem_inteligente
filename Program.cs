24fea57ad690b3919d13caa2e36b668e98304df4
Rafael Gonzaga França <rafael.franca@ad.agnusdei.local>
2026-04-13 09:46:06 -0300

    Funções de Manipulação de Paciente

--- Program.cs
+++ Program.cs
@@ -1,1 +1,17 @@
-﻿Console.WriteLine("Hello, World!");
+﻿class Program
+{
+    static void Main(String[] args)
+    {
+        List<string> pacientes = new List<string>();
+        AdicionarPaciente(pacientes, "Rafael");
+        foreach(string paciente in pacientes)
+        {
+            Console.WriteLine(paciente);
+        }
+    }
+
+    private static void AdicionarPaciente(List<string> pacientes, String nome)
+    {
+        pacientes.Add(nome);
+    }
+}

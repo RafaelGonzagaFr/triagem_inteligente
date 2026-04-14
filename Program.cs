df5375cac155a18976990d462c3ea97db424811d
Rafael Gonzaga França <rafael.franca@ad.agnusdei.local>
2026-04-14 08:12:19 -0300

    Adicionar e Listar pacientes e Menu

--- Program.cs
+++ Program.cs
@@ -3,15 +3,59 @@
     static void Main(String[] args)
     {
         List<string> pacientes = new List<string>();
-        AdicionarPaciente(pacientes, "Rafael");
+        bool sair = false;
+        string? entrada;
+        int escolha;
+        while (!sair)
+        {
+            Menu();
+            entrada = Console.ReadLine();
+            if(!int.TryParse(entrada, out escolha))
+            {
+                Console.WriteLine("Escolha inválida");
+                continue;
+            }
+            switch (escolha)
+            {
+                case 1:
+                    while (true)
+                    {
+                        string? nome;
+                        Console.WriteLine("Nome do paciente: ");
+                        nome = Console.ReadLine();
+                        if(nome == null || nome.Length < 3)
+                        {
+                            Console.WriteLine("Digite um valor válido\n No mínimo 3 caractéres");
+                            continue;
+                        }
+                        AdicionarPaciente(pacientes, nome);
+                        break;
+                    }
+                    break;
+                case 2:
+                    VerListaDePaciente(pacientes);
+                    break;
+            }
+        }
+        
+    }
+
+    private static void AdicionarPaciente(List<string> pacientes, String nome)
+    {
+        pacientes.Add(nome);
+    }
+
+    private static void VerListaDePaciente(List<string> pacientes)
+    {
         foreach(string paciente in pacientes)
         {
             Console.WriteLine(paciente);
         }
     }
 
-    private static void AdicionarPaciente(List<string> pacientes, String nome)
+    private static void Menu()
     {
-        pacientes.Add(nome);
+        Console.WriteLine("1 - Chegada de novo paciente");
+        Console.WriteLine("2 - Ver lista de pacientes");
     }
 }
